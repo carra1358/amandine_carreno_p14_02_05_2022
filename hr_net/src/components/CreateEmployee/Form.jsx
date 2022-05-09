75f9be455641fd78743e44e8707ad74b6fcb5889
amandine carreno <30933638+carra1358@users.noreply.github.com>
2022-05-09 18:40:06 +0200

    add readme

--- hr_net/src/components/CreateEmployee/Form.jsx
+++ hr_net/src/components/CreateEmployee/Form.jsx
@@ -1,7 +1,13 @@
 // import { useForm } from "react-hook-form";
+import { useState } from 'react';
+import DatePicker from 'react-date-picker'
+import { states } from './formState';
 import "./form.scss"
 
 function Form() {
+    const [startDate, setStratDate] = useState();
+    const [birthDate, setBirthDate] = useState();
+
     return (
         <>
             <form>
@@ -13,13 +19,13 @@ function Form() {
                     <label htmlFor="lastName">LastName</label>
                     <input type="text" id="LastName" />
                 </div>
-                <div className="inputs">
+                <div className="input_date">
                     <label htmlFor="birth">Date of Birth</label>
-                    <input type="text" id="Birth" />
+                    <DatePicker onChange={(date) => setBirthDate(date)} value={birthDate} id="Birth" dayPlaceholder='dd' monthPlaceholder="mm" yearPlaceholder="yyyy" />
                 </div>
-                <div className="inputs">
-                    <label htmlFor="Start">Start Date</label>
-                    <input type="text" id="Start" />
+                <div className='input_date'>
+                    <label htmlFor="startDate">Start Date</label>
+                    <DatePicker onChange={(date) => setStratDate(date)} value={startDate} id="startDate" dayPlaceholder='dd' monthPlaceholder="mm" yearPlaceholder="yyyy" />
                 </div>
                 <div className="adress">
                     <h4><span className="deco" />Adress<span className="deco" /></h4>
@@ -33,13 +39,29 @@ function Form() {
                     </div>
                     <div className="inputs">
                         <label htmlFor="state">state</label>
-                        <input type="text" id="state" />
+                        <select id='state'>
+                            {states.map((state) => {
+                                return (
+                                    <option key={state.abbreviation} value={state.name}>{state.name}</option>
+                                )
+                            })}
+                        </select>
                     </div>
                     <div className="inputs">
                         <label htmlFor="zipCode">Zip Code</label>
                         <input type="text" id="zipCode" />
                     </div>
-                    <button type="submit">Save</button>
+                    <div className="inputs">
+                        <label htmlFor="dpt">Departement</label>
+                        <select id='dpt'>
+                            <option>Sales</option>
+                            <option>Marketing</option>
+                            <option>Engineering</option>
+                            <option>Human Resources</option>
+                            <option>Legal</option>
+                        </select>
+                    </div>
+                    <div className='submit_button'><button type="submit">Save</button></div>
                 </div>
             </form>
 
